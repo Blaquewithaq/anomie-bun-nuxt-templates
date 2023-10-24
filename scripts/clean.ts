d962c805df65009e5264f73dfdcf2b2ecf2329c3
Blaquewithaq <blaquewithaq@gmail.com>
2023-10-24 13:47:59 -0500

    chore: update deps and sync

--- scripts/clean.ts
+++ scripts/clean.ts
@@ -1,8 +1,21 @@
+/* eslint-disable no-console */
 import { exec } from "child_process";
 import { promisify } from "util";
 import chalk from "chalk";
 import ora from "ora";
 import { rimraf } from "rimraf";
+
+let full = false;
+
+if (process.argv.length > 2) {
+  switch (process.argv[2]) {
+    case "--full":
+      full = true;
+      break;
+    default:
+      break;
+  }
+}
 
 // Directories and files to clean
 const directories: string[] = [
@@ -14,6 +27,11 @@ const directories: string[] = [
 ];
 
 const files: string[] = [".eslintcache"];
+
+if (full) {
+  directories.push("node_modules");
+  files.push("bun.lockb");
+}
 
 // Clean directories and files
 async function clean() {
@@ -29,13 +47,14 @@ async function prepare() {
 
 // Run
 (async () => {
+  console.log(chalk.cyan("> Anomie CLI"));
   const spinner = ora().start();
   spinner.color = "magenta";
-  spinner.spinner = "aesthetic";
-  spinner.text = chalk.yellow(" Cleaning...");
+  spinner.spinner = "arc";
+  spinner.text = chalk.yellow("cleaning...");
 
   await clean();
   await prepare();
 
-  spinner.succeed(chalk.green("Cleaned"));
+  spinner.succeed(chalk.green("clean"));
 })();
